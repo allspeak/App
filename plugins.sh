dee8c2882c7407c1b861f9ad10283e82247e7ea6
Alberto Inuggi <alberto.inuggi@iit.it>
2017-09-19 13:35:41 +0200

    attempt to may select different models, tf params are now stored in a json that now contains also the contained items (title/id) model labels are not anymore read by a file.
    {
        "sLabel":"controls FSC 23",
        "nInputParams": 792,
        "nContextFrames": 5,
        "nItems2Recognize": 23,
        "sModelFilePath": "controls_fsc.pb",
        "sInputNodeName": "inputs/I",
        "sOutputNodeName": "O",
        "fRecognitionThreshold": 0,
        "nProcessingScheme": 252,
        "sCreationTime": "13092017",
        "items":
        [
            { "title": "Chiama", "id": 1305},
            { "title": "Ciao", "id": 1404},
    		...............................
        ]
    }
    RecognitionCtrl should display the available json to let user change the model....does not work
    
    BUG FIX to nProcessingScheme
    // ===========================================
    // changes' types
    // ===========================================
    NEW FEATURE
    NEW_PLUGIN_METHOD
    NEW_METHOD
    NEW PROPERTIES
    BUG FIX
    OPTIM
    REORG
    RENAMING
    NEXT
    TODO

---
+++ plugins.sh
@@ -0,0 +1,10 @@
+ionic plugin add /data/AllSpeak/CODE/cordova/cordova-plugin-pdialog
+ionic plugin add /data/AllSpeak/CODE/cordova/cordova-plugin-speechrecognition 
+ionic plugin add cordova-plugin-media
+ionic plugin add cordova-plugin-device 
+ionic plugin add cordova-plugin-whitelist     
+ionic plugin add cordova-plugin-android-permissions 
+ionic plugin add cordova-plugin-compat                                                                                                       
+ionic plugin add ionic-plugin-keyboard   
+
+  
